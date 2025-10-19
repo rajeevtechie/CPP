370997985f73f75092ce5181991cf4e161963e52
rajeevgupta2004 <rjv.gpt825@gmail.com>
2025-10-19 13:01:40 +0530

    made few changes

--- Problems/new practise/p_3.cpp
+++ Problems/new practise/p_3.cpp
@@ -1,1 +1,6 @@
 //WAP to perform the addition of two complex numbers using the concept of constructors overloading and a friend function.
+#include<iostream>
+using namespace std;
+int main(){
+    
+}
--- tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-p_back(); //deletes 12 from the vector.
-    // for(int val: vctr){
-    //     cout << val << endl;
-    // }
