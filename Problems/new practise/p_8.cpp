041669f6d70c0abaa1814b09d757990200a38f58
rajeevtechie <rajeev.techie380@gmail.com>
2025-10-23 00:16:25 +0530

    practised hierarchical inheritance

--- Problems/new practise/p_8.cpp
+++ Problems/new practise/p_8.cpp
@@ -16,4 +16,74 @@ public:
 class employee: public person{
 public: 
     int employee_id;
+
+    void get_employee_data(){
+        cout << "Enter the following employee details: " << endl;
+        // cin.ignore(); //ignores the newline character from input buffer.
+        cout << "Name = " ;
+        getline(cin, name);
+        cout << "Age = ";
+        cin >> age;
+        cout << "Gender = ";
+        cin >> gender;
+        cout << "Employee_ID = ";
+        cin >> employee_id;
+    }
+
+    void display_employee_info(){
+        cout << "---Displaying the employee details---" << endl;
+        cout << "Name = " << name << endl;
+        cout << "Age = " << age << endl;
+        cout << "Gender = " << gender << endl;
+        cout << "Employee_ID = " << employee_id << endl;
+    }
 };
+
+class student: public person{
+public:
+    int prn;
+    string branch,batch;
+    float cgpa;
+
+    void get_student_data(){
+        cout << "Enter the following Student details: " << endl;
+        cout << "Name = " ;
+        cin.ignore(); //ignores the newline character from input buffer.
+        getline(cin,name);
+        cout << "Age = ";
+        cin >> age;
+        cout << "Gender = ";
+        cin >> gender;
+        cout << "PRN = ";
+        cin >> prn;
+        cout << "Branch = ";
+        cin >> branch;
+        cout << "Batch = ";
+        cin >> batch;
+        cout << "CGPA = ";
+        cin >> cgpa;
+    }
+
+    void display_student_info(){
+        cout << "---Displaying the student details---" << endl;
+        cout << "Name = " << name << endl;
+        cout << "Age = " << age << endl;
+        cout << "Gender = " << gender << endl;
+        cout << "PRN = " << prn << endl;
+        cout << "Branch = " << branch << endl;
+        cout << "Batch = " << batch << endl;
+        cout << "CGPA = " << cgpa << endl;
+    }
+};
+
+int main(){
+    employee e1;
+    e1.get_employee_data();
+    e1.display_employee_info();
+
+    student s1;
+    s1.get_student_data();
+    s1.display_student_info();
+
+    return 0;
+}
--- Problems/new practise/p_9.cpp
+++ Problems/new practise/p_9.cpp
@@ -4,3 +4,10 @@ Create a base class student that stores the name and PRN of student. Derive two
 classes UT_Marks and sports from the class student. Use the concept of Hybrid
 Inheritance to derive the class result from the class UT_Marks and class sports.
 */
+#include<iostream>
+using namespace std;
+
+int main(){
+    return 0;
+}
+
