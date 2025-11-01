689d5e57b107e4c66d91fa1c9371d399b3922ed8
rajeevtechie <rajeev.techie380@gmail.com>
2025-11-01 23:38:10 +0530

    Practised single linkedlist

---
+++ Problems/new practise/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+rch: " << endl;
+    class list l4;
+    l4.push_back(9);
+    l4.push_back(10);
+    l4.push_back(11);
+    l4.push_back(12);
+    l4.push_back(13);
+    cout << "Index = " << l4.iterati
